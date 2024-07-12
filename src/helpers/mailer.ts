34263ff453275b120b9bdcc2b5358bbe7a6a6a7b
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2024-07-12 10:10:58 +0530

    change code in mailer file

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,22 +1,40 @@
+import User from '@/models/userModel';
 import nodemailer from 'nodemailer'
+import bcryptjs from 'bcryptjs'
 
-export const sendEmail = async ({ email, emailType, userId }:any) => {
+interface Types {
+    email:string,
+    emailType:string,
+    userId:string,
+}
+
+export const sendEmail = async ({ email, emailType, userId }:Types) => {
     try {
-        //TODO : configure mail for usage
+
+        const hashedToken = await bcryptjs.hash(userId.toString(),10)
+
+        if(emailType === "VERIFY")
+        {
+            await User.findByIdAndUpdate(userId,{verifyToken:hashedToken,verifyTokenExpiry:Date.now()+3600000})
+        }
+
+        else if(emailType === "RESET") {
+            await User.findByIdAndUpdate(userId,{forgotPasswordToken:hashedToken,forgotPasswordTokenExpiry:Date.now()+3600000})
+        }
+
         const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, 
+            host: process.env.EMAIL_HOST,
+            port: 2525,
             auth: {
-                user: "maddison53@ethereal.email",
-                pass: "jn7jnAPss4f63QBp6D",
-            },
-        });
+              user: process.env.EMAIL_AUTH_USER,
+              pass: process.env.EMAIL_AUTH_PASS
+            }
+          });
 
         const mailOptions = {
             from: 'savla.deev@gmail.com',
             to: email, 
-            subject: emailType === 'VERIFY' ? "Verify your email" : "Reset your password", 
+            subject: emailType === 'VERIFY' ? "Verify your email" : "Change your password", 
             html: "<b>Hello world?</b>",
         }
 
