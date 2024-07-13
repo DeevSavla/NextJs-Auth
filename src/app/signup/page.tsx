a88877894de4b78a5488de9b33dc0956a4f4f679
Deev Savla <143065962+DeevSavla@users.noreply.github.com>
2024-07-13 11:54:21 +0530

    add login component

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -75,7 +75,8 @@ function SignupPage() {
       />
       <button
         onClick={onSignup}
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisable ? "No signup" : "Signup"}</button>
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisable ? "No signup" : "Signup"}
+      </button>
       <Link href="/login">Visit login page</Link>
     </div>
   )
